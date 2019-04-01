01b5c173e6b766b344ac83b5d6dfcc14bcff9895
Peter Pae <paendabear@gmail.com>
2019-04-01 11:57:14 -0400

    add part 1

---
+++ scripts/store.js
@@ -0,0 +1,22 @@
+/* eslint-disable indent */
+/* eslint-disable strict */
+
+const store = (function() {
+
+    const items = [
+        { id: cuid(), name: 'apples', checked: false },
+        { id: cuid(), name: 'oranges', checked: false },
+        { id: cuid(), name: 'milk', checked: true },
+        { id: cuid(), name: 'bread', checked: false }
+      ]; 
+      let hideChecked = false; 
+      let searchTerm = '';
+    
+    const addItem = function(itemName) {}; const updateItem = function(id, updateData) {}; const toggleHideCheckedFilter = function() {};
+    
+    return { items, hideChecked, addItem, updateItem, toggleHideCheckedFilter };
+    
+    }());
+
+const foo = 'bar';
+
